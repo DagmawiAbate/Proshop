d93058bff4296e0014f7b7ae53d93629204e8715
DagmawiAbate <dagmawiabate2@gmail.com>
2022-08-01 08:39:52 +0300

    Feature profile update added

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,7 +1,18 @@
- import axios from 'axios'
- import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT, USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS } from '../constants/userConstants'
+import axios from 'axios'
+import {
+  USER_DETAILS_FAIL,
+  USER_DETAILS_REQUEST,
+  USER_DETAILS_SUCCESS,
+  USER_LOGIN_FAIL,
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGOUT,
+  USER_REGISTER_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS
+} from '../constants/userConstants'
 
- export const login = (email, password) => async (dispatch) => {
+export const login = (email, password) => async (dispatch) => {
   try {
     dispatch({
       type: USER_LOGIN_REQUEST
@@ -12,7 +23,11 @@
         'Content-Type': 'application/json'
       }
     }
-    const { data } = await axios.post('/api/users/login', {email, password}, config)
+    const { data } = await axios.post(
+      '/api/users/login',
+      { email, password },
+      config
+    )
 
     dispatch({
       type: USER_LOGIN_SUCCESS,
@@ -24,17 +39,19 @@
     dispatch({
       type: USER_LOGIN_FAIL,
       payload:
-        error.response && error.response.data.message ? error.response.data.message : error.message,
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
     })
   }
- }
+}
 
- export const logout = () => (dispatch) => {
+export const logout = () => (dispatch) => {
   localStorage.removeItem('userInfo')
   dispatch({ type: USER_LOGOUT })
- }
+}
 
- export const register = (name, email, password) => async (dispatch) => {
+export const register = (name, email, password) => async (dispatch) => {
   try {
     dispatch({
       type: USER_REGISTER_REQUEST
@@ -45,7 +62,12 @@
         'Content-Type': 'application/json'
       }
     }
-    const { data } = await axios.post('/api/users', {name, email, password}, config)
+
+    const { data } = await axios.post(
+      '/api/users',
+      { name, email, password },
+      config
+    )
 
     dispatch({
       type: USER_REGISTER_SUCCESS,
@@ -62,7 +84,43 @@
     dispatch({
       type: USER_REGISTER_FAIL,
       payload:
-        error.response && error.response.data.message ? error.response.data.message : error.message,
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
     })
   }
- }
+}
+
+export const getUserDetails = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: USER_DETAILS_REQUEST
+    })
+
+    const {
+      userLogin: { userInfo }
+    } = getState()
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${userInfo.token}`
+      }
+    }
+
+    const { data } = await axios.get(`/api/users/${id}`, config)
+
+    dispatch({
+      type: USER_DETAILS_SUCCESS,
+      payload: data
+    })
+  } catch (error) {
+    dispatch({
+      type: USER_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
+    })
+  }
+}
